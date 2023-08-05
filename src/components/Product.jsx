ec38f212ae1e5ba7dca62d365e774865ae41dc35
vinothsmart <s.vinothkanna90@gmail.com>
2023-08-05 08:35:14 +0530

    fallback suspense added

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const Product = () => {
-  return <div>Product</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = useCallback(async () => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+      setIsLoading(false);
+    } catch (_e) {
+      setIsError(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h1>Error</h1>;
+
+  return (
+    <>
+      <h1>Product Dashboard</h1>
+    </>
+  );
 };
 
 export default Product;
