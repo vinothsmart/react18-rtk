7422c8ab63a47d03eb458dc98ebaed608ceb8892
vinothsmart <s.vinothkanna90@gmail.com>
2023-08-09 09:36:24 +0530

    pending

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { lazy, Suspense } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 const Product = lazy(() => import("./components/Product"));
 
 function App() {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route />
+      </Route>,
+    ),
+  );
+
   return (
     <div className="App">
       <Suspense fallback={<h1>App Level Loading...</h1>}>
