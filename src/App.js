376d8f72c18bb5eb10549843686a9e4627c47de5
vinothsmart <s.vinothkanna90@gmail.com>
2023-08-09 15:34:22 +0530

    lazy loading for all components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { lazy, Suspense } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import {
@@ -6,18 +7,43 @@ import {
   RouterProvider,
   Route,
 } from "react-router-dom";
-import Dashboard from "./components/Dashboard";
-import Cart from "./components/Cart";
-import RootLayout from "./components/RootLayout";
-import NoMatch from "./components/NoMatch";
+const Dashboard = lazy(() => import("./components/Dashboard"));
+const Cart = lazy(() => import("./components/Cart"));
+const RootLayout = lazy(() => import("./components/RootLayout"));
+const NoMatch = lazy(() => import("./components/NoMatch"));
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<RootLayout />}>
-        <Route index element={<Dashboard />}></Route>
-        <Route path="/cart" element={<Cart />}></Route>
-        <Route path="*" element={<NoMatch />} />
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<h1>App Level Loading...</h1>}>
+            <RootLayout />
+          </Suspense>
+        }>
+        <Route
+          index
+          element={
+            <Suspense fallback={<h1>App Level Loading...</h1>}>
+              <Dashboard />
+            </Suspense>
+          }></Route>
+        <Route
+          path="/cart"
+          element={
+            <Suspense fallback={<h1>App Level Loading...</h1>}>
+              <Cart />
+            </Suspense>
+          }></Route>
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<h1>App Level Loading...</h1>}>
+              <NoMatch />
+            </Suspense>
+          }
+        />
       </Route>,
     ),
   );
