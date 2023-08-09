74b04aae33ec86bbdd4206b775f2a44fce856fe6
vinothsmart <s.vinothkanna90@gmail.com>
2023-08-09 15:23:31 +0530

    Navbar updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,16 +8,21 @@ const NavBar = () => {
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
         <Navbar.Brand href="#">Redux ToolKit</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll>
-            <Nav.Link to="/" as={Link}>
-              Prodcuts
+        <Nav
+          className="me-auto my-2 my-lg-0"
+          style={{ maxHeight: "100px" }}
+          navbarScroll>
+          <Nav.Link to="/" as={Link}>
+            Prodcuts
+          </Nav.Link>
+        </Nav>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            <Nav.Link to="/cart" as={Link}>
+              My bag 0
             </Nav.Link>
-          </Nav>
+          </Navbar.Text>
         </Navbar.Collapse>
       </Container>
     </Navbar>
