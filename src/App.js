edc55fb2e4753ca03aeec62a5cd620aa9bf3b6d2
vinothsmart <s.vinothkanna90@gmail.com>
2023-08-09 15:29:39 +0530

    products with lazy loading

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { lazy, Suspense } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import {
@@ -11,7 +10,6 @@ import Dashboard from "./components/Dashboard";
 import Cart from "./components/Cart";
 import RootLayout from "./components/RootLayout";
 import NoMatch from "./components/NoMatch";
-const Product = lazy(() => import("./components/Product"));
 
 function App() {
   const router = createBrowserRouter(
@@ -19,13 +17,6 @@ function App() {
       <Route path="/" element={<RootLayout />}>
         <Route index element={<Dashboard />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
-        <Route
-          path="/products"
-          element={
-            <Suspense fallback={<h1>App Level Loading...</h1>}>
-              <Product />
-            </Suspense>
-          }></Route>
         <Route path="*" element={<NoMatch />} />
       </Route>,
     ),
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,12 @@
+import { lazy, Suspense } from "react";
+const Product = lazy(() => import("./Product"));
+
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Suspense fallback={<h1>App Level Loading...</h1>}>
+      <Product />
+    </Suspense>
+  );
 };
 
 export default Dashboard;
