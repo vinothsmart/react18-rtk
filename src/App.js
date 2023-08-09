3422870ebb1593bba5580665b33857b6128407f8
vinothsmart <s.vinothkanna90@gmail.com>
2023-08-09 14:40:52 +0530

    add routes to component

--- src/App.js
+++ src/App.js
@@ -5,23 +5,29 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
+  Route,
 } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Cart from "./components/Cart";
+import RootLayout from "./components/RootLayout";
 const Product = lazy(() => import("./components/Product"));
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route>
-        <Route />
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Dashboard />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
       </Route>,
     ),
   );
 
   return (
     <div className="App">
-      <Suspense fallback={<h1>App Level Loading...</h1>}>
+      {/* <Suspense fallback={<h1>App Level Loading...</h1>}>
         <Product />
-      </Suspense>
+      </Suspense> */}
+      <RouterProvider router={router} />
     </div>
   );
 }
