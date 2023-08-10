7f26dbcf10c163b7db839a31c305056ac68651db
vinothsmart <s.vinothkanna90@gmail.com>
2023-08-10 13:11:41 +0530

    loading and error using slice

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,41 +1,28 @@
 import { useCallback, useEffect, useState } from "react";
+import { Alert } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../store/productSlice";
 import ProductCard from "./ProductCard";
 
 const Product = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
   const dispatch = useDispatch();
-
-  const { data: products } = useSelector(state => state.products);
-
-  // const fetchProducts = useCallback(async () => {
-  //   try {
-  //     const response = await fetch("https://fakestoreapi.com/products");
-  //     const data = await response.json();
-  //     setProducts(data);
-  //     setIsLoading(false);
-  //   } catch (_e) {
-  //     setIsError(true);
-  //   }
-  // }, []);
+  const { data: products, status } = useSelector(state => state.products);
 
   const fetchProducts = useCallback(async () => {
-    try {
-      dispatch(getProducts());
-      setIsLoading(false);
-    } catch (_e) {
-      setIsError(true);
-    }
+    dispatch(getProducts());
   }, [dispatch]);
 
   useEffect(() => {
     fetchProducts();
   }, [fetchProducts]);
 
-  if (isLoading) return <h1>Component Level Loading...</h1>;
-  if (isError) return <h1>Component Level Error</h1>;
+  if (status === "loading") return <h1>Component Level Loading...</h1>;
+  if (status === "failed")
+    return (
+      <Alert variant="danger">
+        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+      </Alert>
+    );
 
   return (
     <>
--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -1,22 +1,35 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   data: [],
+  status: "idle",
 };
 
-export const getProducts = () => async (dispatch, getState) => {
-  const data = await fetch("https://fakestoreapi.com/products");
-  const result = await data.json();
-  dispatch(fetchProducts(result));
-};
+export const getProducts = createAsyncThunk(
+  "products/getProducts",
+  async () => {
+    const data = await fetch("https://fakestoreapi.com/productssss");
+    const result = await data.json();
+    return result;
+  },
+);
 
 const productSlice = createSlice({
   name: "products",
   initialState,
-  reducers: {
-    fetchProducts: (state, action) => {
-      state.data = action.payload;
-    },
+  reducers: {},
+  extraReducers: builder => {
+    builder
+      .addCase(getProducts.pending, state => {
+        state.status = "loading";
+      })
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.status = "idle";
+        state.data = action.payload;
+      })
+      .addCase(getProducts.rejected, state => {
+        state.status = "failed";
+      });
   },
 });
 
