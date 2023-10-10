3d2754005791cdbc73274736840b46a080cf18c0
vinothsmart <s.vinothkanna90@gmail.com>
2023-10-10 19:10:19 +0530

    test data

--- src/components/Testing.jsx
+++ src/components/Testing.jsx
@@ -1,5 +1,32 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+
+const usersDataURL = "https://jsonplaceholder.typicode.com/users";
 
 export const Testing = () => {
-  return <div>Testing</div>;
+  const [list, setList] = useState([]);
+
+  const fetchUsers = useCallback(async () => {
+    const response = await fetch(usersDataURL);
+    const data = await response.json();
+    setList(data);
+  }, []);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
+  return (
+    <main>
+      <h1> List Users Page</h1>
+      {list.map(({ id, name }) => (
+        <div key={id}>
+          <div className="row">
+            <div className="col-lg-4">
+              <p>{name}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+    </main>
+  );
 };
