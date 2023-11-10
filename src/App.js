a76c302735e2d121f99bc8874064280c2b387478
vinothsmart <s.vinothkanna90@gmail.com>
2023-11-10 18:42:17 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const Cart = lazy(() => import("./components/Cart"));
 const RootLayout = lazy(() => import("./components/RootLayout"));
 const NoMatch = lazy(() => import("./components/NoMatch"));
 
+// updated
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
