8b1541b2361a3aac3923f01f02804e5570de5846
vinothsmart <s.vinothkanna90@gmail.com>
2024-01-04 10:43:50 +0530

    up

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -3,7 +3,7 @@ import React, { memo } from "react";
 // const usersDataURL = "https://jsonplaceholder.typicode.com/users";
 
 export const Practice = () => {
-  return <>Page</>;
+  return <h1>Page</h1>;
 };
 
 export default memo(Practice);
