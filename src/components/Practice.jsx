83673147faa1c5bcb973511850fa7878e6ddfd17
vinothsmart <s.vinothkanna90@gmail.com>
2024-01-06 11:18:24 +0530

    wo cp

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { memo, useCallback, useEffect, useState } from "react";
+
+const TODO_API_URL = "https://jsonplaceholder.typicode.com/todos";
 
 const Practice = () => {
-  return <div>Practice</div>;
+  const [todos, setTodos] = useState([]);
+
+  const fetchTodos = useCallback(async () => {
+    const response = await fetch(TODO_API_URL);
+    const result = await response.json();
+    setTodos(result);
+  }, []);
+
+  useEffect(() => {
+    fetchTodos();
+  }, [fetchTodos]);
+
+  const handleRemove = useCallback(
+    id => () => {
+      const removeTodos = todos.filter(todo => todo.id !== id);
+      setTodos(removeTodos);
+    },
+    [todos],
+  );
+
+  return (
+    <>
+      {todos.map(({ id, title }) => (
+        <div key={id}>
+          <p>{title}</p>
+          <button onClick={handleRemove(id)}>remove</button>
+        </div>
+      ))}
+    </>
+  );
 };
 
-export default Practice;
+export default memo(Practice);
