c176ae2a27166266766da121fe6cfb88d8c6484e
vinothsmart <s.vinothkanna90@gmail.com>
2024-01-10 15:22:57 +0530

    rm old code

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,38 +1,7 @@
-import React, { memo, useCallback, useEffect, useState } from "react";
-
-const TODO_API_URL = "https://jsonplaceholder.typicode.com/todos";
+import React from "react";
 
 const Practice = () => {
-  const [todos, setTodos] = useState([]);
-
-  const fetchTodos = useCallback(async () => {
-    const response = await fetch(TODO_API_URL);
-    const result = await response.json();
-    setTodos(result);
-  }, []);
-
-  useEffect(() => {
-    fetchTodos();
-  }, [fetchTodos]);
-
-  const handleRemove = useCallback(
-    id => () => {
-      const removeTodos = todos.filter(todo => todo.id !== id);
-      setTodos(removeTodos);
-    },
-    [todos],
-  );
-
-  return (
-    <>
-      {todos.map(({ id, title }) => (
-        <div key={id}>
-          <p>{title}</p>
-          <button onClick={handleRemove(id)}>remove</button>
-        </div>
-      ))}
-    </>
-  );
+  return <div>Testing</div>;
 };
 
-export default memo(Practice);
+export default Practice;
