8cbe0782cdf4893fd97af11175d2ff789c057718
vinothsmart <s.vinothkanna90@gmail.com>
2024-01-18 20:24:55 +0530

    cart container UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import CardContaier from "./components/CardContaier";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <main>
       <Navbar />
+      <CardContaier />
     </main>
   );
 }
---
+++ src/components/CardContaier.js
@@ -0,0 +1,40 @@
+import CardItem from "./CardItem";
+import { useSelector } from "react-redux";
+
+const CardContaier = () => {
+  const { cartItems, total, amount } = useSelector((store) => store.cart);
+
+  if (amount < 1) {
+    return (
+      <section className="cart">
+        <header>
+          <h2>your bag</h2>
+          <h4 className="empty-cart">is currently empty</h4>
+        </header>
+      </section>
+    );
+  }
+  return (
+    <section className="cart">
+      <header>
+        <h2>your bag</h2>
+      </header>
+      <div>
+        {cartItems.map((item) => {
+          return <CardItem key={item.id} {...item} />;
+        })}
+      </div>
+      <footer>
+        <hr />
+        <div className="cart-total">
+          <h4>
+            total <span>Rs{total}</span>
+          </h4>
+        </div>
+        <button className="btn clear-btn">clear cart</button>
+      </footer>
+    </section>
+  );
+};
+
+export default CardContaier;
