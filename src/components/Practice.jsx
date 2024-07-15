5113d6d8e512932c04e9ac57dce2ca23a1415e9d
vinothsmart <s.vinothkanna90@gmail.com>
2024-07-15 16:33:51 +0530

    todo CRUD

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,33 +1,62 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 
 const Practice = () => {
-  const [users, setUsers] = useState([]);
+  const [email, setEmail] = useState("");
+  const [emailList, setEmailList] = useState([]);
+  const [isAdd, setIsAdd] = useState(true);
 
-  const fetchUsers = useCallback(async () => {
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    const data = await response.json();
-    setUsers(data);
+  const handleChange = useCallback(e => {
+    setEmail(e.target.value);
   }, []);
 
+  const handleAdd = useCallback(() => {
+    setEmailList(prev => [...prev, email]);
+    setEmail("");
+  }, [email]);
+
   const handleDelete = useCallback(
-    id => () => {
-      const filter = users.filter(user => user.id !== id);
-      setUsers(filter);
+    emailId => () => {
+      const filterEmails = emailList.filter(email => email !== emailId);
+      setEmailList(filterEmails);
     },
-    [users],
+    [emailList],
   );
 
-  useEffect(() => {
-    fetchUsers();
-  }, [fetchUsers]);
+  const viewEmail = useCallback(
+    emailId => () => {
+      setEmail(emailId);
+      handleDelete(emailId)();
+      setIsAdd(false);
+    },
+    [handleDelete],
+  );
+
+  const handleEdit = useCallback(
+    selectedEmail => () => {
+      const filterEmails = emailList.filter(email => email !== selectedEmail);
+      setEmailList([...filterEmails, email]);
+      setEmail("");
+      setIsAdd(true);
+    },
+    [email, emailList],
+  );
 
   return (
     <>
-      {users.map(({ id, name }) => (
-        <h1 key={id} onClick={handleDelete(id)}>
-          {name}
-        </h1>
-      ))}
+      <input type="text" name="email" onChange={handleChange} value={email} />
+      <button onClick={isAdd ? handleAdd : handleEdit(email)}>
+        {isAdd ? "Add" : "Edit"}
+      </button>
+      <div>
+        {emailList.map(item => {
+          return (
+            <div key={item}>
+              <h1 onClick={handleDelete(item)}>{item}</h1>
+              <button onClick={viewEmail(item)}>edit</button>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 };
