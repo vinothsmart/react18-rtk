0d8f5690ee5006c2ff7ba4b4f007fbcd4e01a45e
vinothsmart <s.vinothkanna90@gmail.com>
2024-07-15 17:13:26 +0530

    Remove unused state and functions in Practice component

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,64 +1,7 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 
 const Practice = () => {
-  const [email, setEmail] = useState("");
-  const [emailList, setEmailList] = useState([]);
-  const [isAdd, setIsAdd] = useState(true);
-
-  const handleChange = useCallback(e => {
-    setEmail(e.target.value);
-  }, []);
-
-  const handleAdd = useCallback(() => {
-    setEmailList(prev => [...prev, email]);
-    setEmail("");
-  }, [email]);
-
-  const handleDelete = useCallback(
-    emailId => () => {
-      const filterEmails = emailList.filter(email => email !== emailId);
-      setEmailList(filterEmails);
-    },
-    [emailList],
-  );
-
-  const viewEmail = useCallback(
-    emailId => () => {
-      setEmail(emailId);
-      handleDelete(emailId)();
-      setIsAdd(false);
-    },
-    [handleDelete],
-  );
-
-  const handleEdit = useCallback(
-    selectedEmail => () => {
-      const filterEmails = emailList.filter(email => email !== selectedEmail);
-      setEmailList([...filterEmails, email]);
-      setEmail("");
-      setIsAdd(true);
-    },
-    [email, emailList],
-  );
-
-  return (
-    <>
-      <input type="text" name="email" onChange={handleChange} value={email} />
-      <button onClick={isAdd ? handleAdd : handleEdit(email)}>
-        {isAdd ? "Add" : "Edit"}
-      </button>
-      <div>
-        {emailList.map(item => {
-          return (
-            <div key={item}>
-              <h1 onClick={handleDelete(item)}>{item}</h1>
-              <button onClick={viewEmail(item)}>edit</button>
-            </div>
-          );
-        })}
-      </div>
-    </>
-  );
+  return <h1>Testing Page</h1>;
 };
 
 export default Practice;
