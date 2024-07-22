ee29ec0d8dfe11a53bb03ba3ea611eec25fc0c19
vinothsmart <s.vinothkanna90@gmail.com>
2024-07-22 11:20:55 +0530

    Refactor Practice component to remove unused state and functions

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,24 +1,9 @@
-import React, { useCallback, useEffect, useState } from "react";
-
-const USERURL = "https://jsonplaceholder.typicode.com/users";
+import React from "react";
 
 const Practice = () => {
-  const [users, setUsers] = useState([]);
-
-  const fetchUsers = useCallback(async () => {
-    const response = await fetch(USERURL);
-    const users = await response.json();
-    setUsers(users);
-  }, []);
-
-  useEffect(() => {
-    fetchUsers();
-  }, [fetchUsers]);
   return (
     <>
-      {users.map(user => (
-        <h1 key={user.id}>{user.name}</h1>
-      ))}
+      <h1> Testing Page</h1>
     </>
   );
 };
