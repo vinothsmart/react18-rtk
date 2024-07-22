d12d55733717a68c3cb1181b729105c485f21eb9
vinothsmart <s.vinothkanna90@gmail.com>
2024-07-22 11:53:09 +0530

    remove real-time clock functionality

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,18 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Practice = () => {
-  const [currentTime, setCurrnetTime] = useState(
-    new Date().toLocaleTimeString(),
-  );
-
-  useEffect(() => {
-    setInterval(() => {
-      setCurrnetTime(new Date().toLocaleTimeString());
-    }, 1000);
-  }, []);
   return (
     <>
-      <h1> {currentTime}</h1>
+      <h1> Testing Page</h1>
     </>
   );
 };
