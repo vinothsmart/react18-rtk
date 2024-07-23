350917d68d143acc9f847ee9b7223dfdeb181dd6
vinothsmart <s.vinothkanna90@gmail.com>
2024-07-23 12:54:12 +0530

    Refactor Practice component to add login functionality

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,9 +1,53 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 const Practice = () => {
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
+
+  const handleChange = useCallback(e => {
+    const { name, value } = e.target;
+    if (name === "username") {
+      setName(value);
+    } else {
+      setPassword(value);
+    }
+  }, []);
+
+  const handleSubmit = useCallback(e => {
+    e.preventDefault();
+    setIsLogged(true);
+  }, []);
+
+  const handleLogout = useCallback(e => {
+    e.preventDefault();
+    setIsLogged(false);
+  }, []);
+
   return (
     <>
-      <h1> Testing Page</h1>
+      {!isLogged ? (
+        <>
+          <input
+            type="text"
+            name="username"
+            value={name}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+          />
+          <button onClick={handleSubmit}>Login</button>
+        </>
+      ) : (
+        <>
+          <h1>Welcome to Dashaboard Logged in Sucessfully</h1>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      )}
     </>
   );
 };
