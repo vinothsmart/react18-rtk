0d205bd3bbc0aff4a584665d8da33ced75feda73
vinothsmart <s.vinothkanna90@gmail.com>
2024-07-23 13:17:54 +0530

    Refactor Practice component to remove unused state and functions

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,53 +1,9 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 
 const Practice = () => {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLogged, setIsLogged] = useState(false);
-
-  const handleChange = useCallback(e => {
-    const { name, value } = e.target;
-    if (name === "username") {
-      setName(value);
-    } else {
-      setPassword(value);
-    }
-  }, []);
-
-  const handleSubmit = useCallback(e => {
-    e.preventDefault();
-    setIsLogged(true);
-  }, []);
-
-  const handleLogout = useCallback(e => {
-    e.preventDefault();
-    setIsLogged(false);
-  }, []);
-
   return (
     <>
-      {!isLogged ? (
-        <>
-          <input
-            type="text"
-            name="username"
-            value={name}
-            onChange={handleChange}
-          />
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={handleChange}
-          />
-          <button onClick={handleSubmit}>Login</button>
-        </>
-      ) : (
-        <>
-          <h1>Welcome to Dashaboard Logged in Sucessfully</h1>
-          <button onClick={handleLogout}>Logout</button>
-        </>
-      )}
+      <h1>Home Page</h1>
     </>
   );
 };
