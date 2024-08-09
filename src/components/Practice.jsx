996053905535e1a1f7631c89bfbd0e7cd3ee26e0
vinothsmart <s.vinothkanna90@gmail.com>
2024-08-09 18:35:25 +0530

    un completed

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
+const PRODUCT_URL = "https://dummyjson.com/products";
 const Practice = () => {
+  const [searchProduct, setSearchProduct] = useState("");
+
+  const [selectedProduct, setSelectedProduct] = useState({});
+
+  const [userSeachredData, setUserSearchData] = useState([]);
+
+  const [filteredData, setFilteredData] = useState([]);
+
+  const [searchedIds, setSearchedIds] = useState([]);
+
+  const handleChange = useCallback(e => {
+    setSearchProduct(e.target.value);
+  }, []);
+
+  const fetchProductByID = useCallback(async id => {
+    const result = await fetch(PRODUCT_URL + id);
+    const data = await result.json();
+    setSelectedProduct(data);
+    setUserSearchData(prev => [...prev, data]);
+    setSearchedIds(prev => [...prev, id]);
+  }, []);
+
+  useEffect(() => {
+    if (searchProduct !== "") {
+      const checkPrevious = searchedIds.includes(searchProduct.id);
+      if (checkPrevious) {
+        const findByPreviousData = userSeachredData.find(
+          item => item.id === searchProduct.id,
+        );
+        setSelectedProduct(findByPreviousData);
+      } else {
+        fetchProductByID(searchProduct);
+      }
+    } else {
+      setSelectedProduct({});
+    }
+  }, [fetchProductByID, searchProduct, searchedIds, userSeachredData]);
   return (
     <>
-      <h1> Testing Page </h1>
+      <div>
+        <h1>Search Product</h1>
+        <input type="text" value={searchProduct} onChange={handleChange} />
+        {selectedProduct && (
+          <div key={selectedProduct.id}>
+            <h1>{selectedProduct.title}</h1>
+          </div>
+        )}
+      </div>
     </>
   );
 };
