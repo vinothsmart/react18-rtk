3ac63c7b14d76702749abd5fef91c97e1a948d44
vinothsmart <s.vinothkanna90@gmail.com>
2024-08-10 07:52:24 +0530

    Add name capitalization functions to Practice component

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 
 const Practice = () => {
+  const test = "vinoth kanna";
+
+  const nametoUppperCase = sentence => {
+    return sentence
+      .split("")
+      .map(item => item[0].toUpperCase() + item.slice(1))
+      .join("");
+  };
+
+  console.log("vinoth", nametoUppperCase(test));
+  const Captilize = str => {
+    return str
+      .split(" ")
+      .map(word => {
+        return word[0].toUpperCase() + word.slice(1);
+      })
+      .join(" ");
+  };
+  console.log("second", Captilize(test));
   return (
     <>
       <h1> Testing Page </h1>
