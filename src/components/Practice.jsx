9e632df4192c87ce987cff7a803f89df8ce8b33e
vinothsmart <s.vinothkanna90@gmail.com>
2024-08-12 17:06:58 +0530

    Remove unnecessary code and console logs from Practice component

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,38 +1,6 @@
 import React from "react";
 
 const Practice = () => {
-  const test = "vinoth kanna";
-
-  const nametoUppperCase = sentence => {
-    return sentence
-      .split(" ")
-      .map(item => item[0].toUpperCase() + item.slice(1))
-      .join(" ");
-  };
-
-  console.log("vinoth", nametoUppperCase(test));
-  const Captilize = str => {
-    return str
-      .split(" ")
-      .map(word => {
-        return word[0].toUpperCase() + word.slice(1);
-      })
-      .join(" ");
-  };
-  console.log("second", Captilize(test));
-
-  const items = [
-    { name: "east", value: 21 },
-    { name: "south", value: 37 },
-    { name: "atlanta", value: 45 },
-    { name: "Terrain", value: -12 },
-    { name: "Magnetic", value: 13 },
-    { name: "Zone", value: 37 },
-  ];
-
-  const sortItems = items.sort((a, b) => {
-    return a.value - b.value;
-  });
   return (
     <>
       <h1>Testing Page</h1>
