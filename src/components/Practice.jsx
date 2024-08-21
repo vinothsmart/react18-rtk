d68bb930896253cddf8f88778c88a8d564500fdf
vinothsmart <s.vinothkanna90@gmail.com>
2024-08-21 19:51:58 +0530

    refactor: Add search functionality to Practice component

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 const Practice = () => {
+  const numbers = [1, 2, 2, 3, 3, 3, 4];
+
+  const [searchValue, setSearchValue] = useState("");
+  const [filteredValues, setFilteredValues] = useState([]);
+
+  const handleChange = useCallback(e => {
+    setSearchValue(e.target.value);
+  }, []);
+
+  const handleSumbmit = useCallback(
+    e => {
+      e.preventDefault();
+      const filterData = numbers.filter(item => item === +searchValue);
+      setFilteredValues(filterData);
+    },
+    [searchValue],
+  );
   return (
     <>
       <h1>Testing Page</h1>
+      <input type="text" onChange={handleChange} name="searchNumber" />
+      <button onClick={handleSumbmit}>Filter Number</button>
+      <h1>
+        Value searched {searchValue} : {filteredValues.length}
+      </h1>
     </>
   );
 };
