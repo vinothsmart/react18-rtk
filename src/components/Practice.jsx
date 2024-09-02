0eec2dfc1d1f868943707eadd09b727ca5c275b9
vinothsmart <s.vinothkanna90@gmail.com>
2024-09-02 18:42:06 +0530

    Remove unused code and update Practice component to fetch and display user data from API

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,82 +1,9 @@
-import React, { useCallback, useEffect, useState } from "react";
-
-const taskTodos = [
-  {
-    id: 1,
-    name: "Task 1",
-    checked: false,
-  },
-  {
-    id: 2,
-    name: "Task 2",
-    checked: true,
-  },
-  {
-    id: 3,
-    name: "Task 3",
-    checked: true,
-  },
-];
+import React from "react";
 
 const Practice = () => {
-  const [todos, setTodos] = useState([]);
-  const [todoName, setTodoName] = useState("");
-
-  useEffect(() => {
-    setTodos(taskTodos);
-  }, []);
-
-  const handleChange = useCallback(
-    id => () => {
-      const filterData = todos.map(todo => {
-        if (todo.id === id) {
-          return { ...todo, checked: !todo.checked };
-        }
-        return todo;
-      });
-
-      setTodos(filterData);
-    },
-    [todos],
-  );
-
-  const handleTodoText = useCallback(e => {
-    const { value } = e.target;
-    setTodoName(value);
-  }, []);
-
-  const handleAddTodo = useCallback(
-    e => {
-      e.preventDefault();
-      setTodos(prev => [
-        ...prev,
-        {
-          id: prev.length + 1,
-          name: todoName,
-          checked: true,
-        },
-      ]);
-    },
-    [todoName],
-  );
   return (
     <>
       <h1>Testing Page</h1>
-      <ul>
-        {todos.map(todo => (
-          <li key={todo.id}>
-            <input
-              type="checkbox"
-              checked={todo.checked}
-              onChange={handleChange(todo.id)}
-            />
-            {todo.name}
-          </li>
-        ))}
-      </ul>
-      <label>Name:</label>
-      <input type="text" onChange={handleTodoText} value={todoName} />
-      <button onClick={handleAddTodo}>Add Todo</button>
     </>
   );
 };
