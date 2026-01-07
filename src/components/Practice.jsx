b227decd8c0d57eb515861c9ae08b67f1f22cc6e
vinothsmart <s.vinothkanna90@gmail.com>
2026-01-07 13:17:15 +0530

    Practice

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import { useCallback, useEffect, useState } from "react";
+
+const UsersURL = "https://jsonplaceholder.typicode.com/users";
 
 const Practice = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = useCallback(async () => {
+    try {
+      const response = await fetch(UsersURL);
+      const data = await response.json();
+      setUsers(data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchUsers();
+  }, [fetchUsers]);
+
   return (
     <>
       <h1>Testing Page</h1>
+      <>
+        {users.map(user => (
+          <div key={user.id}>
+            {user.name}
+            <button className="m-2 btn-danger">Delete</button>
+          </div>
+        ))}
+      </>
     </>
   );
 };
