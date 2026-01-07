243c9f1123075edcd991dd370f460f146495b5a6
vinothsmart <s.vinothkanna90@gmail.com>
2026-01-07 13:39:38 +0530

    practice

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -4,16 +4,23 @@ const UsersURL = "https://jsonplaceholder.typicode.com/users";
 
 const Practice = () => {
   const [users, setUsers] = useState([]);
-
   const fetchUsers = useCallback(async () => {
     try {
-      const response = await fetch(UsersURL);
-      const data = await response.json();
-      setUsers(data);
-    } catch (error) {
-      console.error("Error fetching users:", error);
+      const data = await fetch(UsersURL);
+      const result = await data.json();
+      setUsers(result);
+    } catch (e) {
+      console.log("Error in fecthing", e);
     }
   }, []);
+
+  const handleDelte = useCallback(
+    deletUser => () => {
+      const filterUers = users.filter(user => user.id !== deletUser);
+      setUsers(filterUers);
+    },
+    [users],
+  );
 
   useEffect(() => {
     fetchUsers();
@@ -22,14 +29,14 @@ const Practice = () => {
   return (
     <>
       <h1>Testing Page</h1>
-      <>
-        {users.map(user => (
-          <div key={user.id}>
-            {user.name}
-            <button className="m-2 btn-danger">Delete</button>
+      {users.map(user => (
+        <>
+          <div>
+            <p>{user.name}</p>
+            <span onClick={handleDelte(user.id)}>Delete</span>
           </div>
-        ))}
-      </>
+        </>
+      ))}
     </>
   );
 };
