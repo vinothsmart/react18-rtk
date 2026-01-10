f679e44f2e9e312285ff65b74cad9851870094e2
vinothsmart <s.vinothkanna90@gmail.com>
2026-01-10 14:20:20 +0530

    we will fix

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,4 +1,58 @@
 const Practice = () => {
+  import {useState, useEffect} from "react"l
+
+
+const UserAnalystics = ({setUserTimeSpend}) => {
+
+
+
+useEffect (() =>{
+  setTimeput (()=> {
+setUserTimeSpend(...prev => prev +1)
+} , 1000}
+},[])}
+
+
+
+}
+
+
+export UserAnalystics;
+
+
+import UserAnalystics form "./UserAnalystics;
+
+
+const HomePage = () => {
+const [userTimeSpend, setUserTimeSpend] =  useState(0);
+const [showData, setShowData] = useState(false);
+
+const handeClick = useCallback(()=>{
+
+
+});
+
+},[]);
+
+
+return (
+
+<div>
+<h1>Home Page</h1>
+<buton onClick={handleClick}> Show user Spend Data </button>
+<UserAnalystics setUserTimeSpend={setUserTimeSpend} />
+
+{
+	showData && (
+		<div>
+		<p> User spends: {userTimeSpend} </p>
+			</div>
+		)
+}
+</div>
+)
+
+export HomePage;
   return (
     <>
       <h1>Testing Page</h1>
