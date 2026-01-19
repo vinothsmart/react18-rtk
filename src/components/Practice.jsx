705a1bdcee9932854bef4bd014a3d64486d55c60
vinothsmart <s.vinothkanna90@gmail.com>
2026-01-19 19:23:53 +0530

    infyosis interview corrected

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,61 +1,24 @@
+import { useState, useCallback } from "react";
+import UserAnalystics from "./UserAnalystics";
+
 const Practice = () => {
-  import {useState, useEffect} from "react"l
+  const [userTimeSpend, setUserTimeSpend] = useState(0);
+  const [showData, setShowData] = useState(false);
 
+  const handeClick = useCallback(() => {
+    setShowData(prev => !prev);
+  }, []);
 
-const UserAnalystics = ({setUserTimeSpend}) => {
-
-
-
-useEffect (() =>{
-  setTimeput (()=> {
-setUserTimeSpend(...prev => prev +1)
-} , 1000}
-},[])}
-
-
-
-}
-
-
-export UserAnalystics;
-
-
-import UserAnalystics form "./UserAnalystics;
-
-
-const HomePage = () => {
-const [userTimeSpend, setUserTimeSpend] =  useState(0);
-const [showData, setShowData] = useState(false);
-
-const handeClick = useCallback(()=>{
-
-
-});
-
-},[]);
-
-
-return (
-
-<div>
-<h1>Home Page</h1>
-<buton onClick={handleClick}> Show user Spend Data </button>
-<UserAnalystics setUserTimeSpend={setUserTimeSpend} />
-
-{
-	showData && (
-		<div>
-		<p> User spends: {userTimeSpend} </p>
-			</div>
-		)
-}
-</div>
-)
-
-export HomePage;
   return (
     <>
       <h1>Testing Page</h1>
+      <button onClick={handeClick}> Show user Spend Data </button>
+      <UserAnalystics setUserTimeSpend={setUserTimeSpend} />
+      {showData && (
+        <div>
+          <p> User spends: {userTimeSpend} </p>
+        </div>
+      )}
     </>
   );
 };
