3f5642dd84bc1b59d6089407f0963f6513f15613
vinothsmart <s.vinothkanna90@gmail.com>
2026-02-04 13:17:05 +0530

    TCS Feb4 interview

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,7 +1,53 @@
+import { useCallback, useState } from "react";
+
 const Practice = () => {
+  // let arr1 = [1, 2, 2, 3, 3, 3, 4];
+  // let arr2 = [1, 2, 3, 4, 5, 6, 7];
+
+  // const unique = arr1.indexOf(arr1);
+  // console.log({ unique });
+  const [values, setValues] = useState({
+    rows: "",
+    cloumns: "",
+  });
+
+  const handleChange = useCallback(e => {
+    const { name, value } = e.target;
+    setValues(preVale => ({
+      ...preVale,
+      [name]: value,
+    }));
+  }, []);
+
+  const addRowAndColumns = useCallback(() => {
+    console.log("values", values);
+  }, [values]);
+
   return (
     <>
       <h1>Testing Page</h1>
+      <div>
+        <input
+          type="text"
+          name="rows"
+          value={values.rows}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          name="cloumns"
+          value={values.cloumns}
+          onChange={handleChange}
+        />
+      </div>
+      <button onClick={addRowAndColumns}>Add Rows ad Columns</button>
+      <table>
+        {values.rows}
+        <tr>ROW</tr>
+        <td>Columns</td>
+      </table>
     </>
   );
 };
