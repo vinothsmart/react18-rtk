b49a20cc532bf11f97a881cf0df7b1a87f67af2e
vinothsmart <s.vinothkanna90@gmail.com>
2026-02-04 10:39:51 +0530

    merge after rm

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,24 +1,7 @@
-import { useState, useCallback } from "react";
-import UserAnalystics from "./UserAnalystics";
-
 const Practice = () => {
-  const [userTimeSpend, setUserTimeSpend] = useState(0);
-  const [showData, setShowData] = useState(false);
-
-  const handeClick = useCallback(() => {
-    setShowData(prev => !prev);
-  }, []);
-
   return (
     <>
       <h1>Testing Page</h1>
-      <button onClick={handeClick}> Show user Spend Data </button>
-      <UserAnalystics setUserTimeSpend={setUserTimeSpend} />
-      {showData && (
-        <div>
-          <p> User spends: {userTimeSpend} </p>
-        </div>
-      )}
     </>
   );
 };
