39f9c52e57a9dfbc18b7de463d5e260b665d5f2a
vinothsmart <s.vinothkanna90@gmail.com>
2026-05-11 13:54:12 +0530

    tcs inteview clear

--- src/components/Practice.jsx
+++ src/components/Practice.jsx
@@ -1,65 +1,11 @@
-import { useCallback, useState } from "react";
+import { memo } from "react";
 
 const Practice = () => {
-  // let arr1 = [1, 2, 2, 3, 3, 3, 4];
-  // let arr2 = [1, 2, 3, 4, 5, 6, 7];
-
-  // const unique = arr1.indexOf(arr1);
-  // console.log({ unique });
-  const [values, setValues] = useState({
-    rows: "",
-    cloumns: "",
-  });
-
-  const handleChange = useCallback(e => {
-    const { name, value } = e.target;
-    setValues(preVale => ({
-      ...preVale,
-      [name]: value,
-    }));
-  }, []);
-
-  const addRowAndColumns = useCallback(() => {
-    console.log("values", values);
-  }, [values]);
-
   return (
     <>
       <h1>Testing Page</h1>
-      <div>
-        <input
-          type="text"
-          name="rows"
-          value={values.rows}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <input
-          type="text"
-          name="cloumns"
-          value={values.cloumns}
-          onChange={handleChange}
-        />
-      </div>
-      <button onClick={addRowAndColumns}>Add Rows ad Columns</button>
-      <table>
-        {/* {values.rows} */}
-        {/* dynamic row and colums based on values values.row and values.cloumns */}
-        {[...Array(Number(values.rows))].map((_, rowIndex) => (
-          <tr key={rowIndex}>
-            {[...Array(Number(values.cloumns))].map((_, colIndex) => (
-              <td
-                key={colIndex}
-                style={{ border: "1px solid black", padding: "5px" }}>
-                Row {rowIndex + 1} - Col {colIndex + 1}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </table>
     </>
   );
 };
 
-export default Practice;
+export default memo(Practice);
